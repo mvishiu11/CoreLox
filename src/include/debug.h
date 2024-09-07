693301bec8386d3393493de577df6903cc7136c6
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-09-07 14:13:24 +0200

    feat: basic workcode

---
+++ src/include/debug.h
@@ -0,0 +1,9 @@
+#ifndef carbonlox_debug_h
+#define carbonlox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
