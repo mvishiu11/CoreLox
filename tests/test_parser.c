24bdb309236b99442a1cbccf1ca1af4f624efc7f
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-09-07 14:13:03 +0200

    feat: setup tests

---
+++ tests/test_parser.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+
+#include "parser.h"
+
+void test_basic_parsing() {
+  assert(1 == 1);  // Placeholder
+}
+
+int main() {
+  test_basic_parsing();
+  printf("All tests passed!\n");
+  return 0;
+}
