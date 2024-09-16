7ec985fa2d0201b7c3cacb043132224030a8bdc4
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-09-16 11:52:18 +0200

    fix: correctly resolve names in compiler stack

--- src/compiler.c
+++ src/compiler.c
@@ -102,6 +102,7 @@ static void emitConstant(Value value) {
 static void emitReturn() { emitByte(OP_RETURN); }
 
 static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->enclosing = current;
   compiler->function = NULL;
   compiler->type = type;
   compiler->localCount = 0;
@@ -114,6 +115,10 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
   compiler->locals = GROW_ARRAY(Local, NULL, 0, compiler->localCapacity);
   compiler->function = newFunction();
   current = compiler;
+  if (type != TYPE_SCRIPT) {
+    current->function->name = copyString(parser.previous.start,
+                                         parser.previous.length);
+  }
 
   Local* local = &current->locals[current->localCount++];
   local->depth = 0;
@@ -133,6 +138,7 @@ static ObjFunction* endCompiler() {
   }
 #endif
 
+  current = current->enclosing;
   return function;
 }
 
@@ -320,6 +326,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0) return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -333,6 +340,54 @@ static void defineVariable(uint8_t global) {
 }
 
 //< Variable Parsing Functions
+//> Function Parsing Functions
+
+static void function(FunctionType type) {
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope(); 
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+  if (!check(TOKEN_RIGHT_PAREN)) {
+    do {
+      current->function->arity++;
+      if (current->function->arity > 255) {
+        errorAtCurrent("Can't have more than 255 parameters.");
+      }
+      uint8_t constant = parseVariable("Expect parameter name.");
+      defineVariable(constant);
+    } while (match(TOKEN_COMMA));
+  }
+
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction* function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
+}
+
+static uint8_t argumentList() {
+  uint8_t argCount = 0;
+  if (!check(TOKEN_RIGHT_PAREN)) {
+    do {
+      expression();
+      argCount++;
+    } while (match(TOKEN_COMMA));
+  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
+  return argCount;
+}
+
+//< Function Parsing Functionss
 //> Control Flow Parsing Functions
 
 static void and_(bool canAssign __attribute__((unused))) {
@@ -551,7 +606,9 @@ static void continueStatement() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
@@ -627,6 +684,11 @@ static void binary(bool canAssign __attribute__((unused))) {
     default:
       return;  // Unreachable.
   }
+}
+
+static void call(bool canAssign __attribute__((unused))) {
+  uint8_t argCount = argumentList();
+  emitBytes(OP_CALL, argCount);
 }
 
 static void ternary(bool canAssign __attribute__((unused))) {
@@ -774,7 +836,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]    = {variable, NULL,    PREC_NONE},
     [TOKEN_IF]            = {NULL,     NULL,    PREC_NONE},
     [TOKEN_LEFT_BRACE]    = {NULL,     NULL,    PREC_NONE},
-    [TOKEN_LEFT_PAREN]    = {grouping, NULL,    PREC_NONE},
+    [TOKEN_LEFT_PAREN]    = {grouping, call,   PREC_CALL},
     [TOKEN_LESS]          = {NULL,     binary,  PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]    = {NULL,     binary,  PREC_COMPARISON},
     [TOKEN_MINUS]         = {unary,    binary,  PREC_TERM},
